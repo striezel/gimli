2f0b8e9e6d0642530d315af719bd68dbb8eb9780
Dirk Stolle <striezel-dev@web.de>
2022-03-10 02:53:28 +0100

    tests: add test cases for gandalf-the-grey

---
+++ tests/apps/gandalf_the_grey/no-arguments.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Script to test executable when started with no arguments.
+#
+#  Copyright (C) 2022  Dirk Stolle
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# get first parameter
+if [[ -z $1 ]]
+then
+  echo "First parameter must be executable file!"
+  exit 1
+fi
+
+EXECUTABLE="$1"
+"$EXECUTABLE"
+if [ $? -ne 1 ]
+then
+  echo "Executable did not exit with code 1."
+  exit 1
+fi
+
+exit 0
