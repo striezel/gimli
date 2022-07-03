9ec336ed62c89a81508d57c695f5f3093f17bcad
Dirk Stolle <striezel-dev@web.de>
2022-07-03 17:43:14 +0200

    change license of gandalf_the_grey from GNU LGPL v3 to GNU GPL v3
    The license of all the other example programs is GNU LGPL v3,
    too, so this merely preserves consistency.
    
    Note that the library part itself is still licensed under the
    GNU LGPL v3.

--- apps/gandalf_the_grey/changelog.md
+++ apps/gandalf_the_grey/changelog.md
@@ -4,6 +4,15 @@ _(Note: This changelog focuses on the major changes between the different
 versions. Therefore, it may not contain all changes. Especially smaller fixes or
 improvements may be omitted.)_
 
+## Version 0.2.1 (2022-07-03)
+
+The program's license is changed from GNU Lesser General Public License,
+version 3, to the GNU General Public License, version 3.
+
+This was the original intention in the first place. While the library parts of
+gimli are licensed under GNU LGPL v3, the example applications are licensed
+under GNU GPL v3.
+
 ## Version 0.2.0 (2022-03-12)
 
 Support for Bitmap images is added.
--- apps/gandalf_the_grey/main.cpp
+++ apps/gandalf_the_grey/main.cpp
@@ -4,16 +4,16 @@
     Copyright (C) 2022  Dirk Stolle
 
     This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as published by
+    it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Lesser General Public License for more details.
+    GNU General Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public License
+    You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  -------------------------------------------------------------------------------
 */
@@ -35,7 +35,7 @@
 
 void showVersion()
 {
-  std::cout << "gandalf-the-grey, version 0.2.0, 2022-03-12\n"
+  std::cout << "gandalf-the-grey, version 0.2.1, 2022-07-03\n"
             << "\n"
             << "Library versions:" << std::endl
             << "  * Boost " << (BOOST_VERSION / 100000) << "." << ((BOOST_VERSION / 100) % 1000) << "." << (BOOST_VERSION % 100) << std::endl
