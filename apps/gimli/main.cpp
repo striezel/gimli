26272a486a2b6b5c7c114f5c5a9d2fe960bb8a84
Dirk Stolle <striezel-dev@web.de>
2025-01-20 01:36:32 +0100

    gimli: fix potentially wrong size for file names on Windows
    The wide-character string that is passed to the path constructor
    has potentially several null characters at the end. In practice
    that should not be a problem, because C-like OS APIs stop reading
    strings at the first null character. But let's not take any
    chances here.
    
    Linux builds are not affected by this problem.

--- apps/gimli/main.cpp
+++ apps/gimli/main.cpp
@@ -1,7 +1,7 @@
 /*
  -------------------------------------------------------------------------------
     This file is part of the Generic Image Library (gimli).
-    Copyright (C) 2022, 2024  Dirk Stolle
+    Copyright (C) 2022, 2024, 2025  Dirk Stolle
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -33,7 +33,7 @@
 
 void showVersion()
 {
-  std::cout << "gimli, version 0.2.1, 2024-04-14\n"
+  std::cout << "gimli, version 0.2.2, 2025-01-20\n"
             << "\n";
   library_versions();
   showLicenseInformation();
@@ -137,7 +137,7 @@ int main(int argc, char** argv)
             std::cerr << "Error: " << param << " is not a valid parameter!\n";
             return rcInvalidParameter;
           }
-          file = utf16;
+          file = utf16.substr(0, num_chars);
           #endif
         }
         catch (...)
